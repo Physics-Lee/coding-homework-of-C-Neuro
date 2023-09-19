6f093e1777adf6aa1bba540b6d6cfaad9987fea7
Physics-Lee <hyperdunk2019@mail.ustc.edu.cn>
2023-09-19 16:47:12 +0800

    use length to the father
    use length to the father instead of mean length when calculate spine reach zone

--- visualization of a neuron/code/visualization_of_a_neuron.m
+++ visualization of a neuron/code/visualization_of_a_neuron.m
@@ -12,6 +12,8 @@ switch file
     case 3
         data_imported = importdata('../data/3 arbor from larval zebrafish.txt');
 end
+
+% allocate
 segment_index = data_imported(:,1);
 segment_type = data_imported(:,2);
 X = data_imported(:,3);
@@ -19,7 +21,9 @@ Y = data_imported(:,4);
 Z = data_imported(:,5);
 segment_diameter = data_imported(:,6);
 father_segment_index = data_imported(:,7);
-length_of_point = length(X);
+
+% disp
+number_of_points = length(X);
 fprintf("The number of the point is %d \n",length(X));
 
 %% draw graph
@@ -55,9 +59,9 @@ switch flag
                 title('arbors of a larval zebrafish');
         end
         
-        % 按父子关系对散点图进行连线
-        for i = 1:length_of_point
-            for j = 1:length_of_point
+        % draw lines by parent-child relationship
+        for i = 1:number_of_points
+            for j = 1:number_of_points
                 if father_segment_index(j) == segment_index(i) % If point i is the father of point j
                     line( [X(i) X(j)],[Y(i) Y(j)],[Z(i) Z(j)] );
                 end
@@ -75,8 +79,8 @@ switch flag
         k = 0; % count variable
         number_of_2_child_branching_point = 0;
         number_of_3_child_branching_point = 0;
-        for i = 1:length_of_point
-            for j = 1:length_of_point
+        for i = 1:number_of_points
+            for j = 1:number_of_points
                 if father_segment_index(j) == segment_index(i) % If point i is the father of point j
                     k = k+1;
                 end
@@ -89,30 +93,34 @@ switch flag
             end
             k = 0; % reset
         end
-        fprintf('至少有2个孩子的节点的个数为%d \n 至少有3个孩子的节点的个数为%d \n',number_of_2_child_branching_point,number_of_3_child_branching_point);
+        fprintf('The number of nodes with at least 2 children: %d \n The number of nodes with at least 3 children: %d \n',...
+            number_of_2_child_branching_point,number_of_3_child_branching_point);
     case 0
 end
 
 %% calculate the average path length from a dendrite segment to the soma
-length_to_soma = zeros(1,length_of_point);
-count = 3;
-for i = 3:length_of_point
-    for j = 1:length_of_point
-        if father_segment_index(i) == segment_index(j) % If point j is the father of point i
-            length_to_father = ((X(i)-X(j))^2+(Y(i)-Y(j))^2+(Z(i)-Z(j))^2)^(1/2); % Euclid metric
-            length_to_soma(count) = length_to_father + length_to_soma(j);
-            count = count + 1;
-            break; % a point only has 1 father so we can use break;
+length_to_father = zeros(1,number_of_points);
+for i = 3:number_of_points
+    for j = 1:number_of_points
+
+        % If point j is the father of point i
+        if father_segment_index(i) == segment_index(j)
+
+            % Euclidean Metric/Euclidean Distance
+            length_to_father(i) = ((X(i)-X(j))^2+(Y(i)-Y(j))^2+(Z(i)-Z(j))^2)^(1/2);
+
+            % a point only has 1 father
+            break;
+
         end
+
     end
 end
-average_length = mean(length_to_soma(3:length_of_point));
-fprintf("The average path length from a dendrite segment to the soma is %.2f um \n", average_length);
 
 %% calculate the spine reach zone of the Purkinje dendrite
-length_total = length_of_point * average_length;
+length_total = sum(length_to_father);
 s = 2; % um
-V_spine_reach_zone = length_total * pi * s^2; % 这样算出来的是上限，但我一时没想到如何去掉重复的
+V_spine_reach_zone = length_total * pi * s^2; % um^3
 switch file
     case 1
         fprintf("The spine reach zone of the pyramidal dendrite is %.2f um^3 \n", V_spine_reach_zone);
@@ -123,14 +131,14 @@ switch file
 end
 
 %% calculate the 3D convex hull
-[graph_of_convex_hull,volume_of_convex_hull] = convhull(X,Y,Z);
+[graph_of_convex_hull,V_convex_hull] = convhull(X,Y,Z);
 switch file
     case 1
-        fprintf("The volume of convex hull of the pyramidal dendrite is %.2f um^3 \n", volume_of_convex_hull);
+        fprintf("The volume of convex hull of the pyramidal dendrite is %.2f um^3 \n", V_convex_hull);
     case 2        
-        fprintf("The volume of convex hull of the Purkinje dendrite is %.2f um^3 \n", volume_of_convex_hull);
+        fprintf("The volume of convex hull of the Purkinje dendrite is %.2f um^3 \n", V_convex_hull);
     case 3
-        fprintf("The volume of convex hull of the arbor from larval zebrafish is %.2f um^3 \n", volume_of_convex_hull);
+        fprintf("The volume of convex hull of the arbor from larval zebrafish is %.2f um^3 \n", V_convex_hull);
 end
 figure(2)
 trisurf(graph_of_convex_hull,X,Y,Z,'FaceColor','cyan')
@@ -140,6 +148,9 @@ axis equal;
 xlabel('x axis');
 ylabel('y axis');
 zlabel('z axis');
+
+% disp ratio
+fprintf("Ratio: %.4f \n",V_spine_reach_zone/V_convex_hull)
 
 %% linear Sholl plot
 prompt = "Do you want to do a linear Sholl analysis?" + newline + "1: Yes" + newline + "2: No \n";
@@ -160,23 +171,27 @@ switch flag
         number_of_lines_puncture_the_sphere = zeros(1,length(radius_of_the_sphere));
         Euclid_metric = @(x,y,z) ((x - X(1))^2 + (y - Y(1))^2 + (z - Z(1))^2)^(1/2); % metric is a fashionable name of distance.
         for k = 1:length(radius_of_the_sphere)
-            for i = 1:length_of_point
-                for j = 1:length_of_point % 肯定不用跑遍O(n^2)，但我不想化简了，反正也花不了几分钟
+            for i = 1:number_of_points
+                for j = 1:number_of_points % can be faster than O(n^2)
                     if father_segment_index(j) == segment_index(i) % i is the father of j
                         d_i = Euclid_metric(X(i),Y(i),Z(i));
                         d_j = Euclid_metric(X(j),Y(j),Z(j));
-                        if (d_i^2 - radius_of_the_sphere(k)^2) * (d_j^2 - radius_of_the_sphere(k)^2) < 0 % 父亲节点和儿子节点一个在里一个在外
+
+                        % The father node and the son node are one in and one out
+                        if (d_i^2 - radius_of_the_sphere(k)^2) * (d_j^2 - radius_of_the_sphere(k)^2) < 0
                             count = count + 1;
                         end
+
                     end
                 end
             end
             number_of_lines_puncture_the_sphere(k) = count;
             count = 0; % reset
         end
+        figure;
         plot(radius_of_the_sphere,number_of_lines_puncture_the_sphere);
         xlabel('radius of the sphere (\mum)');
-        ylabel('number of lines puncture the sphere');
+        ylabel('number of lines puncturing the sphere');
         title('linear Sholl plot');
     case 0
 end
